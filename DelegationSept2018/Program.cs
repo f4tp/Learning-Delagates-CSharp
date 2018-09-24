69f3a71523ed5e666da0b6e207f035c4e9c94653
f4tp <mr_treadwell@live.co.uk>
2018-09-24 15:34:23 +0100

    Add project files.

---
+++ DelegationSept2018/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegationSept2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
