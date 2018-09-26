60ecaec8a94d33a5d14d022cf4028585e9a8a210
f4tp <mr_treadwell@live.co.uk>
2018-09-26 10:59:37 +0100

    Some comments added about the benefits of delegates

--- DelegationSept2018/Program.cs
+++ DelegationSept2018/Program.cs
@@ -4,7 +4,24 @@ namespace DelegationSept2018
 {
     class Program
     {
+        #region
         //https://www.youtube.com/watch?v=jQgwEsJISy0
+        //this video was used to write thsi program
+        //it told me that you can add many method calls to the delegate object
+        //and call it once to call many methods, which is where the benefit is (observer pattern)...
+        //... when many subscribers need to be notified of changes
+        //it also told me that they are important in calling events
+        //~TODO retrace and follow the method call stack
+
+        //https://www.youtube.com/watch?v=ifbYA8hyvjc&t=320s
+        // a delegate is a pointer / reference to a method...
+        //a delegate is an object which can hold a reference to call a method of your choice.
+        //define delagate
+        //create new delegate object
+        //pass it the method name you want it to call
+        //call it by saying delagateInstanceName.Invoke();
+        //the above informe dme that teh benefit of delagates was in callbacks?
+        #endregion
 
         static void Main(string[] args)
         {
